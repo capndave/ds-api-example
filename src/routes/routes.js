b4a384e5800e7f134329bd4a1b4fd1dd76203b98
capndave <capndavet@gmail.com>
2019-12-17 16:45:45 -0600

    Wrote endpoints;

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,39 +1,45 @@
 const router = require('express').Router()
-// const sqlServer = require('../db/sqlServer')
 const logger = require('../logger/logger')
+// TODO: add controllers for each route
 
-/* From Admin App */
-// router.get('/appraisers', sqlServer.sp('ma_get_appraisers')) // Hit by admin app
-router.get('/', (request, response) => {
+/* GET */
+
+router.get('/getPropIdsInPanel', (req, res) => {
   response.status(200).send('Hello World')
 })
-router.get('/appraisers', (req, res) => {
-//   postgres.get
-//     .appraisers()
-//     .then(data => {
-//       sendResponse.success(res, data)
-//     })
-//     .catch(e => {
-//       logger.error(e)
-//       sendResponse.error(res, e)
-//     })
+
+router.get('/getBoardMembers', (req, res) => {
+  response.status(200).send('Hello World')
 })
-// router.get('/panels/admin', sqlServer.sp('fa_get_panels_admin'))
-// router.get('/panel/:panel_num', sqlServer.spReqBody('fa_get_single_panel'))
-// router.get(
-//   '/properties/:prop_id',
-//   sqlServer.spReqBody('fa_get_single_property')
-// )
-// router.get('/queue/admin', sqlServer.sp('fa_get_queue_admin'))
-// router.get('/queue/waitroom', sqlServer.sp('fa_get_queue_waitroom'))
-// // router.get('/queue', sqlServer.sp('get_queue'))
 
-// /* From Signin App */
-// router.get('/dockets/:arbq_id', sqlServer.getDocket('ma_get_docket'))
-// // router.get('/source/:arbq_id', sqlServer.spReqBody('get_source_data'))
+router.get('/getAffidavitEntered', (req, res) => {
+  response.status(200).send('Hello World')
+})
 
-// /* For Panels App */
-// // router.get('/formal/panels/admin', sqlServer.sp('fa_get_panels_admin'))
-// router.get('/panel/:panel_num', sqlServer.spReqBody('fa_get_single_panel'))
+router.get('/getProtestYears', (req, res) => {
+  response.status(200).send('Hello World')
+})
+
+router.get('/getProtestData', (req, res) => {
+  response.status(200).send('Hello World')
+})
+
+router.get('/getAppraisers', (req, res) => {
+  response.status(200).send('Hello World')
+})
+
+/* POST */
+
+router.post('/postBoardMemberNamesAndSignatures', (req, res) => {
+  response.status(200).send('Hello World')
+})
+
+router.post('/postFormData', (req, res) => {
+  response.status(200).send('Hello World')
+})
+
+router.post('/confirmFormData', (req, res) => {
+  response.status(200).send('Hello World')
+})
 
 module.exports = router
