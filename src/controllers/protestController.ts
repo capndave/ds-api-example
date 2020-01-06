9233c00344574b9208aa0428d62ee17e0dbd8f1e
capndave <capndavet@gmail.com>
2020-01-06 12:25:35 -0600

    added postProtestData method

--- src/controllers/protestController.ts
+++ src/controllers/protestController.ts
@@ -26,22 +26,38 @@ exports.getProtestYears = async function (req: Request, res: Response) {
 }
 
  /**
-  * Extracts the prop id and connection pool from the request object,
-  * gets data from the getProtestYearsFromDatabase service,
+  * Extracts the prop id, year and connection pool from the request object,
+  * gets data from the getProtestDataFromDatabase service,
   * and sends data with the response object
   * @async
   * @method
   * @param { Request } req
   * @param { Response } res
   */
-exports.getProtestData
 
-// TODO: Extract prop id AND YEAR from request query string
+exports.getProtestData = async function (req: Request, res: Response) {
     const propId: number = parseInt(req.params.propId)
     const year: number = parseInt(req.query.year)
     const pool: any = req.app.locals.pool
 
-    const {recordset: data}: number[] = await getProtestDataFromDatabase({panel, pool})
+    const {recordset: data}: number[] = await getProtestDataFromDatabase({panel, year, pool})
+    res.send(data).status(200)
+
+}
+ 
+/**
+  * Extracts the prop id, year and connection pool from the request object,
+  * and posts data to the postProtestDataToDatabase service
+  * @async
+  * @method
+  * @param { Request } req
+  * @param { Response } res
+  */
+exports.postProtestData = async function (req: Request, res: Response) {
+
+    const pool: any = req.app.locals.pool
+
+    const {recordset: data}: number[] = await postProtestDataToDatabase(req.body, pool)
     res.send(data).status(200)
 
 }
