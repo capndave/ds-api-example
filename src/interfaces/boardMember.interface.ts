9f7faa24d4a34354bcdfd36d93f7433bb7b33767
capndave <capndavet@gmail.com>
2020-01-13 08:02:27 -0600

    converted FullNameIdAndSignature to an interface from a class

--- src/interfaces/boardMember.interface.ts
+++ src/interfaces/boardMember.interface.ts
@@ -1,5 +1,10 @@
 export interface IBoardMember {
-  board_member_id: number,
+  board_member_id: number
   full_name: string
 }
-  
+
+export interface FullNameIdAndSignature {
+  board_member_id: number
+  full_name: string
+  signature: Buffer
+}
--- src/models/board/boardMember.model.ts
+++ src/models/board/boardMember.model.ts
@@ -31,15 +31,3 @@ export class FullNameAndId {
       this.full_name = this.full_name
     }
 }
-
-export class FullNameIdAndSignature {
-    board_member_id: number
-    full_name: string
-    signature: Buffer
-
-    constructor(input?: any) {
-      this.board_member_id = input.board_member_id
-      this.full_name = input.full_name
-      this.signature = input.signature
-    }
-}
