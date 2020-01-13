bcd313081f689ab6b46f200514fa9c737169b619
capndave <capndavet@gmail.com>
2020-01-13 09:20:31 -0600

    fixed missing parameter def

--- src/services/protest/getProtestDataFromDatabase.ts
+++ src/services/protest/getProtestDataFromDatabase.ts
@@ -1,6 +1,7 @@
-import logger from '../../logger/logger'
 import path from 'path'
 import sql from 'mssql'
+import logger from '../../logger/logger'
+import { ConnectionPool } from 'mssql'
 
 const fileName = path.basename(__filename)
 
@@ -8,17 +9,18 @@ const fileName = path.basename(__filename)
   * Get all protest data for a particular property ID and year from the database.
   * @async
   * @function getProtestDataFromDatabase
-  * @param { object } pool - A sql connection pool
+  * @param { ConnectionPool } pool - A sql connection pool
   * @returns { Promise<object> }
   */
-export default async function getProtestDataFromDatabase(pool: any): Promise<number[]> {
+export default async function getProtestDataFromDatabase(propId: number, year: number, pool: ConnectionPool): Promise<number[]> {
   // TODO: Find correct sp name after db stuff has been figured out
   try {
-    return await pool
+    const { recordset } = await pool
       .request()
       .input('propId', sql.Int, propId)
       .input('year', sql.Int, year)
       .execute('ma_get_protest_data')
+    return recordset
   } catch (error) {
     logger.error(
       `Error fetching data from database in ${fileName} [23]: ${error}`
