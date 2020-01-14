a033463def2981d3df42a47392da96711ef4ef35
capndave <capndavet@gmail.com>
2020-01-14 08:13:47 -0600

    added input fields to postProtestDataToDatabase

--- src/interfaces/protest.interface.ts
+++ src/interfaces/protest.interface.ts
@@ -1,3 +1,25 @@
 export interface IPropValYear {
     prop_val_yr: number
 }
+
+export interface IProtest {
+    prop_id: number 
+    prop_val_yr: number
+    prot_taxes_paid: boolean
+    late_protest_approved: boolean
+    late_protest_denial_reason: string
+    withdrawn: boolean
+    dismissed: boolean
+    prot_affidavit_testimony_recieved: boolean
+    moved_to_informal?: boolean
+    phone_hearing: boolean
+    code_25_25_c: boolean
+    code_25_25_d: boolean
+    code_41_411: boolean
+    ag_hearing: boolean
+    exemption_hearing: boolean
+    value_hearing: boolean
+    market_value: number
+    equity_value: number
+    protest_status: string
+}
