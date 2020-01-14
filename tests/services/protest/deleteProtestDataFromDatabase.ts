c8c44dd93864cc3f21d1dc2931b54857b25b2337
capndave <capndavet@gmail.com>
2020-01-14 11:24:25 -0600

    wrote deleteProtestDataFromDatabase helper

---
+++ tests/services/protest/deleteProtestDataFromDatabase.ts
@@ -0,0 +1,30 @@
+import path from 'path'
+import { ConnectionPool, IResult, Int } from 'mssql'
+import logger from '../../../src/logger/logger'
+
+const fileName = path.basename(__filename)
+
+/**
+ * Get all protest data for a particular property ID and year from the database.
+ * @async
+ * @function deleteProtestData
+ * @param { ConnectionPool } pool - A sql connection pool
+ * @returns { Promise<object> }
+ */
+export default async function getProtestDataFromDatabase(
+  propId: number,
+  year: number,
+  pool: ConnectionPool
+): Promise<void> {
+  try {
+    const { recordset } = await pool
+      .request()
+      .input('prop_id', Int, propId)
+      .input('prop_val_yr', Int, year)
+      .execute('ma_delete_protest_data')
+  } catch (error) {
+    throw new Error(
+      `Error fetching data from database in ${fileName} [23]: ${error}`
+    )
+  }
+}
