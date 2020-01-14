cd9f87237cf100b7b4ce78f698b6af37ecb54d63
capndave <capndavet@gmail.com>
2020-01-14 16:55:36 -0600

    added formData class to formsController

--- src/controllers/formsController.ts
+++ src/controllers/formsController.ts
@@ -1,9 +1,9 @@
-
 import { Request, Response } from 'express'
 import { ConnectionPool } from 'mssql'
 import { IFullNameAndId } from '../interfaces'
 import {
-  { CommunicationAffidavit, DecisionSheet }
+  CommunicationAffidavit,
+  DecisionSheet
 } from '../services/forms/makeForm'
 import getSignatureFiles from '../services/board/getSignatureFiles'
 import saveSignatureFile from '../services/board/saveSignatureFile'
@@ -22,9 +22,20 @@ import prettyPrintObject from '../services/utilities/prettyPrintObject'
  * @remarks testing
  */
 export async function getForms(req: Request, res: Response) {
+  const propId: number = parseInt(req.params.propId)
+  const year: number = parseInt(req.params.year)
   const pool: ConnectionPool = req.app.locals.pool
-  const namesAndIds: IFullNameAndId[] = await getAllBoardMemberNamesAndIdsFromDatabase(
-    pool
-  )
-  res.send(namesAndIds).status(200)
+  class FormData {
+      prop_id: number,
+      prop_val_yr: number
+
+      constructor(input: any) {
+          this.prop_id = input.propId
+          this.prop_val_yr = input.prop_val_yr
+      }
+  }
+  const data = new FormData(req.params)
+  const forms: any = await DecisionSheet.generate(data)
+  console.log(forms)
+//   res.send(namesAndIds).status(200)
 }
