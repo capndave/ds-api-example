219d4090a852e60aca3c35c524e26907238c47ba
capndave <capndavet@gmail.com>
2020-01-22 10:44:38 -0600

    added communicationAffidavit template

--- src/interfaces/protest.interface.ts
+++ src/interfaces/protest.interface.ts
@@ -22,4 +22,21 @@ export interface IProtest {
     protest_status: string
     value_hearing: boolean
     withdrawn: boolean
+
+    bpp_recommended_market_value: number
+    bpp_recommended_equal_appraised_value: number
+    assessed_recommended_market_value: number
+    assessed_recommended_equal_appraised_value: number
+    market_recommended_market_value: number
+    market_recommended_equal_appraised_value: number
+    case_id?: number
+    owner?: string
+    legal_desc?: string
+    description?: string
+    situs?: string
+    agent_id?: number
+    agent_name?: number
+    agent_authority_to_protest?: boolean
+    agent_authority_to_resolve?: boolean
+    date?: string
 }
